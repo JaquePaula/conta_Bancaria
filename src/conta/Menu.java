bd215b463a122391b40d1f3e734756b18b663366
JaquePaula <jaquemarques.santos@gmail.com>
2023-12-15 19:32:58 -0300

    add contacorrente e contapoupanca

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -2,28 +2,48 @@ package conta;
 
 import java.util.Scanner;
 import conta.model.Conta;
-import conta.util.Cores; 
+import conta.util.Cores;
+import conta.model.ContaCorrente;
+import conta.model.ContaPoupança;
 
 public class Menu {
 
 	public static void main(String[] args) {
-		// Foi instanciado o Objeto c1, da Classe Conta, através do Método Construtor. Para chamar o Método Construtor, foi utilizada a palavra reservada new.
-		// Teste da Classe Conta 
-				Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);
-				c1.visualizar(); // O Objeto c1, da Classe Conta, chama o Método visualizar(), que exibirá no console todos os dados do Objeto.
-				c1.sacar(12000.0f);
-				c1.visualizar();
-				c1.depositar(5000.0f);
-				c1.visualizar();
+		// Foi instanciado o Objeto c1, da Classe Conta, através do Método Construtor.
+		// Para chamar o Método Construtor, foi utilizada a palavra reservada new.
+		// Teste da Classe Conta
+		Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);
+		c1.visualizar(); // O Objeto c1, da Classe Conta, chama o Método visualizar(), que exibirá no
+							// console todos os dados do Objeto.
+		c1.sacar(12000.0f);
+		c1.visualizar();
+		c1.depositar(5000.0f);
+		c1.visualizar();
+
+		// Teste da Classe Conta Corrente
+		ContaCorrente cc1 = new ContaCorrente(2, 123, 1, "Mariana", 15000.0f, 1000.0f);
+		cc1.visualizar();
+		cc1.sacar(12000.0f);
+		cc1.visualizar();
+		cc1.depositar(5000.0f);
+		cc1.visualizar();
+
+		// Teste da Classe Conta Poupança
+		ContaPoupança cp1 = new ContaPoupança(3, 123, 2, "Victor", 100000.0f, 15);
+		cp1.visualizar();
+		cp1.sacar(1000.0f);
+		cp1.visualizar();
+		cp1.depositar(5000.0f);
+		cp1.visualizar();
 
 		Scanner leia = new Scanner(System.in);
 
 		int opcao;
 
 		while (true) {
-			// true, pois ira ficar em loop infinito até que a opção 9 seja digitada. 
+			// true, pois ira ficar em loop infinito até que a opção 9 seja digitada.
 
-			System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND 
+			System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND
 					+ "*****************************************************");
 			System.out.println("                                                     ");
 			System.out.println("                BANCO DO BRAZIL COM Z                ");
@@ -47,7 +67,7 @@ public class Menu {
 			opcao = leia.nextInt();
 
 			if (opcao == 9) {
-				System.out.println(Cores.TEXT_WHITE_BOLD +"\nBanco do Brazil com Z - O seu Futuro começa aqui!");
+				System.out.println(Cores.TEXT_WHITE_BOLD + "\nBanco do Brazil com Z - O seu Futuro começa aqui!");
 				sobre();
 				leia.close();
 				System.exit(0);
@@ -55,39 +75,39 @@ public class Menu {
 
 			switch (opcao) {
 			case 1:
-				System.out.println(Cores.TEXT_WHITE_BOLD +"Criar Conta\n\n");
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Criar Conta\n\n");
 
 				break;
 			case 2:
-				System.out.println(Cores.TEXT_WHITE_BOLD +"Listar todas as Contas\n\n");
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Listar todas as Contas\n\n");
 
 				break;
 			case 3:
-				System.out.println(Cores.TEXT_WHITE_BOLD +"Consultar dados da Conta - por número\n\n");
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Consultar dados da Conta - por número\n\n");
 
 				break;
 			case 4:
-				System.out.println(Cores.TEXT_WHITE_BOLD +"Atualizar dados da Conta\n\n");
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Atualizar dados da Conta\n\n");
 
 				break;
 			case 5:
-				System.out.println(Cores.TEXT_WHITE_BOLD +"Apagar a Conta\n\n");
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Apagar a Conta\n\n");
 
 				break;
 			case 6:
-				System.out.println(Cores.TEXT_WHITE_BOLD +"Saque\n\n");
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Saque\n\n");
 
 				break;
 			case 7:
-				System.out.println(Cores.TEXT_WHITE_BOLD +"Depósito\n\n");
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Depósito\n\n");
 
 				break;
 			case 8:
-				System.out.println(Cores.TEXT_WHITE_BOLD +"Transferência entre Contas\n\n");
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Transferência entre Contas\n\n");
 
 				break;
 			default:
-				System.out.println(Cores.TEXT_RED_BOLD +"\nOpção Inválida!\n" + Cores.TEXT_RESET);
+				System.out.println(Cores.TEXT_RED_BOLD + "\nOpção Inválida!\n" + Cores.TEXT_RESET);
 				break;
 			}
 		}
