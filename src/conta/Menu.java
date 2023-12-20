8d22c67cf8dec16b458920d401b28ac783972d8f
JaquePaula <jaquemarques.santos@gmail.com>
2023-12-20 16:09:48 -0300

    add contaController and updates class Menu

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -3,6 +3,7 @@ package conta;
 import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import conta.controller.ContaController;
 import conta.model.Conta;
 import conta.util.Cores;
 import conta.model.ContaCorrente;
@@ -10,37 +11,31 @@ import conta.model.ContaPoupança;
 
 public class Menu {
 
-	
-
 	public static void main(String[] args) {
-		
+
+		ContaController contas = new ContaController();
+
 		Scanner leia = new Scanner(System.in);
-		int opcao;
 
-		/*
-		 * Foi instanciado o Objeto c1, da Classe Conta, através do Método Construtor.
-		 * // Para chamar o Método Construtor, foi utilizada a palavra reservada new. //
-		 * Teste da Classe Conta Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);
-		 * c1.visualizar(); // O Objeto c1, da Classe Conta, chama o Método
-		 * visualizar(), que exibirá no // console todos os dados do Objeto.
-		 * c1.sacar(12000.0f); c1.visualizar(); c1.depositar(5000.0f); c1.visualizar();
-		 */
+		int opcao, numero, agencia, tipo, aniversario;
+		String titular;
+		float saldo, limite;
+		
+		System.out.println("\n Criar Contas\n");
+		
+		ContaCorrente cc1 = new ContaCorrente(contas.gerarNumero(), 123, 1, "João da Silva", 1000f, 100.0f);
+		contas.cadastrar(cc1);
 
-		// Teste da Classe Conta Corrente
-		ContaCorrente cc1 = new ContaCorrente(2, 123, 1, "Mariana", 15000.0f, 1000.0f);
-		cc1.visualizar();
-		cc1.sacar(12000.0f);
-		cc1.visualizar();
-		cc1.depositar(5000.0f);
-		cc1.visualizar();
+		ContaCorrente cc2 = new ContaCorrente(contas.gerarNumero(), 124, 1, "Maria da Silva", 2000f, 100.0f);
+		contas.cadastrar(cc2);
 
-		// Teste da Classe Conta Poupança
-		ContaPoupança cp1 = new ContaPoupança(3, 123, 2, "Victor", 100000.0f, 15);
-		cp1.visualizar();
-		cp1.sacar(1000.0f);
-		cp1.visualizar();
-		cp1.depositar(5000.0f);
-		cp1.visualizar();
+		ContaPoupança cp1 = new ContaPoupança(contas.gerarNumero(), 125, 2, "Mariana dos Santos", 4000f, 12);
+		contas.cadastrar(cp1);
+
+		ContaPoupança cp2 = new ContaPoupança(contas.gerarNumero(), 125, 2, "Juliana Ramos", 8000f, 15);
+		contas.cadastrar(cp2);
+
+		contas.listarTodas();
 
 		while (true) {
 			// true, pois ira ficar em loop infinito até que a opção 9 seja digitada.
@@ -73,23 +68,48 @@ public class Menu {
 				leia.nextLine();
 				opcao = 0;
 			}
-
 			if (opcao == 9) {
 				System.out.println(Cores.TEXT_WHITE_BOLD + "\nBanco do Brazil com Z - O seu Futuro começa aqui!");
 				sobre();
 				leia.close();
 				System.exit(0);
 			}
-
 			switch (opcao) {
 			case 1:
 				System.out.println(Cores.TEXT_WHITE_BOLD + "Criar Conta\n\n");
 
+				System.out.println("Digite o Numero da Agência: ");
+				agencia = leia.nextInt();
+				System.out.println("Digite o nome do titular: ");
+				leia.skip("\\R?"); // ignora quebra de linha, para permitir que o next line leia palavras compostas
+				titular = leia.nextLine();
+
+				do {
+					System.out.println("Digite o Tipo de Conta (1-CC ou 2-CP): ");
+					tipo = leia.nextInt();
+				} while (tipo < 1 && tipo > 2);
+
+				System.out.println("Digite o Saldo da Conta (R$): ");
+				saldo = leia.nextFloat();
+				// switch expression é o uso do -> 
+				switch (tipo) {
+				case 1 -> {
+					System.out.println("Digite o limite de crédito (R$): ");
+					limite = leia.nextFloat();
+					contas.cadastrar(new ContaCorrente(contas.gerarNumero(), agencia, tipo, titular, saldo, limite));
+				}
+				case 2 -> {
+					System.out.println("Digite o dia do Aniversário da Conta: ");
+					aniversario = leia.nextInt();
+					contas.cadastrar(new ContaPoupança(contas.gerarNumero(), agencia, tipo, titular, saldo, aniversario));
+				}
+				}
+				
 				keyPress();
 				break;
 			case 2:
 				System.out.println(Cores.TEXT_WHITE_BOLD + "Listar todas as Contas\n\n");
-
+				contas.listarTodas();
 				keyPress();
 				break;
 			case 3:
@@ -142,7 +162,7 @@ public class Menu {
 	public static void keyPress() {
 		try {
 			System.out.println(Cores.TEXT_RESET + "\n\nPressione Enter para Continuar...");
-			System.in.read(); //indica a leitura de um dispositivo de entrada padrão (teclado)
+			System.in.read(); // indica a leitura de um dispositivo de entrada padrão (teclado)
 
 		} catch (IOException e) {
 
